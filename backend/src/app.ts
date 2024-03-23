9713c321f47d2096ebc19fce163c7f2acc4f345c
equinhone <equinhone@gmail.com>
2024-03-23 15:59:59 -0400

    init

---
+++ backend/src/app.ts
@@ -0,0 +1,78 @@
+import "./bootstrap";
+import "reflect-metadata";
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import * as Sentry from "@sentry/node";
+
+import "./database";
+import uploadConfig from "./config/upload";
+import AppError from "./errors/AppError";
+import routes from "./routes";
+import { logger } from "./utils/logger";
+import { messageQueue, sendScheduledMessages } from "./queues";
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+
+const app = express();
+
+app.set("queues", {
+  messageQueue,
+  sendScheduledMessages
+});
+
+/*app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});*/
+
+app/*.use(
+  cors({
+    //credentials: true,
+    //origin: '*'
+    //origin: process.env.FRONTEND_URL
+  })
+);*/
+
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-  With, Content-Type, Accept");
+  next();   
+});*/
+
+console.log("testando o cors")
+
+app.use(
+  cors({   
+    origin: ['http://localhost:3000', 'https://demo.amezap.com.br'],
+    //origin: 'http://localhost:3000' , 
+    credentials :  true,  
+    methods: 'GET,PUT,POST,DELETE,OPTIONS', 
+    allowedHeaders: 'Content-Type,Authorization' 
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use("/public", express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(async (err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    logger.warn(err);
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  logger.error(err);
+  return res.status(500).json({ error: "Internal server error" });
+});
+
+export default app;
