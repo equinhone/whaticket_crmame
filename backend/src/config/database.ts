01c29fe77ad2d3b6909e6ddb7741796db03ce78d
equinhone <equinhone@gmail.com>
2024-03-26 10:30:22 -0400

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -18,6 +18,6 @@ module.exports = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: process.env.DB_DEBUG === "true"
+  logging:  process.env.DB_DEBUG === "true"
 };
 
